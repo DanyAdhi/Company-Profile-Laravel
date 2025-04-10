dd5ce1ac91b8215b424e8de1b32a0ddba9383fc1
danyadhi <danyadhi4149@gmail.com>
2025-04-10 14:32:56 +0700

    Upgrate laravel version 9

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,7 +3,9 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
+// use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class TrustProxies extends Middleware
 {
@@ -19,5 +21,10 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    // protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers =
+        SymfonyRequest::HEADER_X_FORWARDED_FOR |
+        SymfonyRequest::HEADER_X_FORWARDED_HOST |
+        SymfonyRequest::HEADER_X_FORWARDED_PORT |
+        SymfonyRequest::HEADER_X_FORWARDED_PROTO;
 }
